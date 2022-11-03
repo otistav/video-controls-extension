df065a8290cd81cad8e3e8d627d9c1b2111315f5
Sam Bocharov <semen_bard@mail.ru>
2022-11-03 20:45:52 +0200

    speed controls

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,46 @@
 chrome.action.onClicked.addListener((tab) => {
   console.log(tab, 'tab');
 });
+
+const updated = async (tabId: any, info: any) => {
+  if (info.status === 'complete') {
+    const tab = await chrome.tabs.get(tabId);
+    console.log(tab, 'tab');
+    if (!(tab as any).url.includes("chrome://")) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id as number, allFrames: true },
+        files: ['./content.js'],
+        // func: () => {
+        //   console.log('bebebe hui', window.location)
+        //   const videoElements = document.getElementsByTagName('video');
+        //   console.log(videoElements, 'in background');
+        //   let mutationObserver = new MutationObserver(mutations => {
+        //     const videoElements = document.getElementsByTagName('video');
+        //     console.log(videoElements, 'bg elems', window.location.href);
+        //     const elems = document.getElementsByTagName('video');
+        //     console.log(elems, 'in background');
+        //     //for (let i=0; i<videoElements.length; i++) {
+        //     // videoElements[i].parentElement!.insertBefore(createControlsPanel(), videoElements[i]);
+        //     // }
+        //   });
+        //   mutationObserver.observe(document.body, { childList: true })
+        // }
+      });
+    }
+  }
+}
+
+chrome.tabs.onUpdated.addListener(updated);
+// (async () => {
+//   let queryOptions = { active: true, lastFocusedWindow: true };
+//   // `tab` will either be a `tabs.Tab` instance or `undefined`.
+//   const [tab] = await chrome.tabs.query(queryOptions);
+//   console.log(tab, 'tab');
+//   if (!(tab as any).url.includes("chrome://")) {
+//     chrome.scripting.executeScript({
+//       target: { tabId: tab.id as number, allFrames: true },
+//       files: ['./content.js'],
+//     });
+//   }
+// })()
+
--- src/content.ts
+++ src/content.ts
@@ -1,3 +1,58 @@
-window.onload = () => {
-  console.log('hello world');
+// video.setAttribute('playbackRate', rate);
+/*
+ 1. Create empty container on each video
+ 2. Make this container appear only when video is focused
+ 3. Create button to control speed
+*/
+
+const createSpeedButton = (speed: number, video: HTMLVideoElement): HTMLDivElement => {
+  const button = document.createElement('div');
+  button.innerHTML = speed.toString();
+  button.style.paddingLeft = '2px';
+  button.style.height = '100%';
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log('clicking element', video);
+    video.playbackRate = speed;
+    video.play();
+  });
+  return button;
+}
+const createControlsPanel = (video: HTMLVideoElement) => {
+  const panel = document.createElement('div');
+  panel.style.width = '100px';
+  panel.style.height = '20px';
+  panel.style.backgroundColor = 'rgba(209, 233, 144, 0.4)';
+  panel.style.position = 'absolute';
+  panel.style.right = '0px';
+  panel.style.zIndex = '10000000';
+  panel.style.display = 'flex';
+  panel.className = 'hehey';
+  const speed125 = createSpeedButton(1.25, video);
+  const speed2 = createSpeedButton(2, video);
+  panel.appendChild(speed125);
+  panel.appendChild(speed2);
+  return panel;
 };
+const panelExists = (video: HTMLVideoElement): boolean => {
+  if (video.previousElementSibling?.className === 'hehey') return true;
+  return false;
+}
+const createVideoPanels = () => {
+  const videoElements = document.getElementsByTagName('video');
+  for (let i = 0; i < videoElements.length; i++) {
+    const video = videoElements[i];
+    console.log(video, 'video');
+    const hasTrack = Array.from(video.children).find((c) => c.tagName === 'TRACK');
+    if (!panelExists(video) && (video.src || hasTrack)) {
+      video.parentElement!.insertBefore(createControlsPanel(video), video);
+    }
+  }
+}
+createVideoPanels();
+let mutationObserver = new MutationObserver(mutations => {
+  console.log('mutating');
+  createVideoPanels();
+});
+mutationObserver.observe(document.body, { childList: true })
