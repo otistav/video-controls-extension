4c066a4aa5d12f08b28235fab1a15ae6ab00ac77
Sam Bocharov <semen_bard@mail.ru>
2022-11-07 18:27:29 +0200

    iframes

--- src/background.ts
+++ src/background.ts
@@ -10,37 +10,27 @@ const updated = async (tabId: any, info: any) => {
       chrome.scripting.executeScript({
         target: { tabId: tab.id as number, allFrames: true },
         files: ['./content.js'],
-        // func: () => {
-        //   console.log('bebebe hui', window.location)
-        //   const videoElements = document.getElementsByTagName('video');
-        //   console.log(videoElements, 'in background');
-        //   let mutationObserver = new MutationObserver(mutations => {
-        //     const videoElements = document.getElementsByTagName('video');
-        //     console.log(videoElements, 'bg elems', window.location.href);
-        //     const elems = document.getElementsByTagName('video');
-        //     console.log(elems, 'in background');
-        //     //for (let i=0; i<videoElements.length; i++) {
-        //     // videoElements[i].parentElement!.insertBefore(createControlsPanel(), videoElements[i]);
-        //     // }
-        //   });
-        //   mutationObserver.observe(document.body, { childList: true })
-        // }
       });
     }
   }
 }
 
 chrome.tabs.onUpdated.addListener(updated);
-// (async () => {
-//   let queryOptions = { active: true, lastFocusedWindow: true };
-//   // `tab` will either be a `tabs.Tab` instance or `undefined`.
-//   const [tab] = await chrome.tabs.query(queryOptions);
-//   console.log(tab, 'tab');
-//   if (!(tab as any).url.includes("chrome://")) {
-//     chrome.scripting.executeScript({
-//       target: { tabId: tab.id as number, allFrames: true },
-//       files: ['./content.js'],
-//     });
-//   }
-// })()
 
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+  // TODO: discover why few seconds later frame is being added
+  setTimeout(async () => {
+    console.log(sender?.tab?.id, 'idd');
+    const frames = await chrome.webNavigation.getAllFrames(
+      {
+        tabId: sender?.tab?.id as number,
+      },
+    )
+    console.log(frames, 'frames')
+    chrome.scripting.executeScript({
+      target: { tabId: sender?.tab?.id as number, frameIds: frames?.map(frame => frame.frameId) },
+      files: ['./content.js'],
+    });
+  }, 3000)
+
+});
--- src/content.ts
+++ src/content.ts
@@ -5,18 +5,28 @@
  3. Create button to control speed
 */
 
+const SPEED_ELEMS = [1, 1.25, 1.5, 2, 4];
+console.log('CONTENT LOADED HERE', window.location.href)
+const handleVideoClick = (e: MouseEvent) => {
+  console.log(e.target, 'target');
+}
+
+document.addEventListener('click', handleVideoClick);
+const onSpeedChange = (e: MouseEvent, video: HTMLVideoElement, speed: number) => {
+  e.preventDefault();
+  e.stopPropagation();
+  console.log('clicking element', video);
+  video.playbackRate = speed;
+  video.play();
+}
+
 const createSpeedButton = (speed: number, video: HTMLVideoElement): HTMLDivElement => {
   const button = document.createElement('div');
   button.innerHTML = speed.toString();
   button.style.paddingLeft = '2px';
   button.style.height = '100%';
-  button.addEventListener('click', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    console.log('clicking element', video);
-    video.playbackRate = speed;
-    video.play();
-  });
+  button.style.cursor = 'pointer';
+  button.addEventListener('click', (e) => onSpeedChange(e, video, speed));
   return button;
 }
 const createControlsPanel = (video: HTMLVideoElement) => {
@@ -29,30 +39,50 @@ const createControlsPanel = (video: HTMLVideoElement) => {
   panel.style.zIndex = '10000000';
   panel.style.display = 'flex';
   panel.className = 'hehey';
-  const speed125 = createSpeedButton(1.25, video);
-  const speed2 = createSpeedButton(2, video);
-  panel.appendChild(speed125);
-  panel.appendChild(speed2);
+  for (const speed of SPEED_ELEMS) {
+    const elem = createSpeedButton(speed, video);
+    panel.appendChild(elem);
+  }
   return panel;
 };
 const panelExists = (video: HTMLVideoElement): boolean => {
   if (video.previousElementSibling?.className === 'hehey') return true;
   return false;
 }
+
+const createPanel = (video: HTMLVideoElement) => {
+  const hasTrack = Array.from(video.children).find((c) => c.tagName === 'TRACK');
+  if (!panelExists(video) && (video.src || hasTrack)) {
+    video.parentElement!.insertBefore(createControlsPanel(video), video);
+  }
+}
 const createVideoPanels = () => {
   const videoElements = document.getElementsByTagName('video');
   for (let i = 0; i < videoElements.length; i++) {
     const video = videoElements[i];
-    console.log(video, 'video');
-    const hasTrack = Array.from(video.children).find((c) => c.tagName === 'TRACK');
-    if (!panelExists(video) && (video.src || hasTrack)) {
-      video.parentElement!.insertBefore(createControlsPanel(video), video);
-    }
+    let videoObserver = new MutationObserver(mutations => {
+      console.log(mutations, 'mutations');
+      for (let mutation of mutations) {
+        console.log('mutation', mutation)
+        if (mutation.attributeName === 'src') {
+          console.log('src changed', video);
+          createPanel(video);
+        }
+      }
+    });
+    videoObserver.observe(video, { attributes: true })
+    createPanel(video);
   }
 }
 createVideoPanels();
 let mutationObserver = new MutationObserver(mutations => {
-  console.log('mutating');
+  console.log('mutating', mutations);
+  for (const mutation of mutations) {
+    if (Array.from(mutation.addedNodes).find(node => node.nodeName === 'IFRAME')) {
+      console.log('iframe added');
+      chrome.runtime.sendMessage({ shouldSendState: true });
+    }
+  }
   createVideoPanels();
 });
-mutationObserver.observe(document.body, { childList: true })
+mutationObserver.observe(document.body, { childList: true, subtree: true, characterData: true })
